e00a6dac7a3edd2296a979fa9df14f1470eb6885
Pedro Gonoring <53987520+pedrogonoring1@users.noreply.github.com>
2020-06-14 10:41:04 -0300

    Atualizacao do card

--- src/pages/ParadasProximas.vue
+++ src/pages/ParadasProximas.vue
@@ -3,14 +3,14 @@
 
     <!-- Title -->
     <div class="row">
-        <div class="col-12 q-pt-lg q-mb-lg">
+        <div class="col-12 q-pt-lg q-mb-lg q-mt-sm">
             <label class="text-subtitle1 text-weight-bolder q-ml-lg q-mt-lg"> PARADAS PRÓXIMAS </label>
         </div>
     </div>  
 
 
     <!-- Card para filtrar local de parada -->
-    <div class="row text-center q-pr-sm">
+    <div class="row text-center q-pr-sm q-mt-sm">
         <div class="col-4">
             <q-img
                 src="~assets/img/hotel.png"
@@ -51,34 +51,88 @@
     </div>
 
 
-    <!-- Painel de Pontos próximos do motorista -->
-    <div class="row paddingPanel">
-        
-        <div class="col-12 userInfo text-subtitle1">
-            <label class="text-weight-bolder"> 
-                Posto Point Brasil
-            </label> <br>
-            
-            <label> 15 km de você. </label>
 
-            <div class="text-right">
-                <label> 786 curtidas </label>
-                <q-img
-                src="~assets/img/iconLike.png"
-                spinner-color="white"
-                style="height: 30px; max-width: 30px"
-                class="q-mb-sm"
-            />
-            </div>
+    <!-- Cards Pontos Proximos -->
+
+    <div class="row text-center">
+        <div class="col-12 q-mt-md">
+            <label class="text-subtitle1 text-weight-bolder">LOCAIS PRÓXIMOS DE VOCÊ</label>
+        </div>
+    </div>
+
+
+    <!-- Cards -->
+
+    <div class="q-pa-lg row items-start q-gutter-md q-pl-md">
+
+    <q-card class="my-card col">
+      <q-img src="https://cdn.quasar.dev/img/chicken-salad.jpg" />
+
+      <q-card-section>
+        <q-btn
+          fab
+          color="primary"
+          icon="place"
+          class="absolute"
+          style="top: 0; right: 12px; transform: translateY(-50%);"
+        />
+
+        <div class="row no-wrap items-center">
+          <div class="col text-h6 ellipsis">
+            Parada Bom Prato
+          </div>
+          <div class="col-auto text-primary text-caption q-pt-md row no-wrap items-center">
+            <q-icon name="place" />
+            150 Km
+          </div>
         </div>
 
+        <q-rating v-model="stars" :max="5" size="23px" />
+      </q-card-section>
+
+      </q-card>
+    </div>
 
 
+    <div class="q-pa-lg row items-start q-gutter-md q-pl-md">
+
+    <q-card class="my-card col">
+      <q-img src="https://i0.wp.com/www.transamerica.com.br/blog/wp-content/uploads/2018/07/Esplanada-1.jpg?resize=870%2C576&ssl=1" />
+
+      <q-card-section>
+        <q-btn
+          fab
+          color="primary"
+          icon="place"
+          class="absolute"
+          style="top: 0; right: 12px; transform: translateY(-50%);"
+        />
+
+        <div class="row no-wrap items-center">
+          <div class="col text-h6 ellipsis">
+            Hotel Sono Bom
+          </div>
+          <div class="col-auto text-primary text-caption q-pt-md row no-wrap items-center">
+            <q-icon name="place" />
+            176 Km
+          </div>
+        </div>
+
+        <q-rating v-model="starsTest" :max="5" size="23px" />
+      </q-card-section>
+
+      </q-card>
     </div>
 
 
 
 
+    <!-- Espaço para o final de página -->
+    <div class="row q-mt-lg">
+        <div class="col-12 q-mt-lg">
+            <div class="q-mt-lg"></div>
+        </div>
+    </div>
 
 
 
@@ -88,6 +142,12 @@
 <script>
 
 export default {
-  name: 'PageParadasProximas'
+  name: 'PageParadasProximas',
+  data () {
+    return {
+      stars: 4,
+      starsTest: 5,
+    }
+  }
 }
 </script>
