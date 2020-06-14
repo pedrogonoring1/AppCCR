bfaf1ce5b1c20ded64092bbb6e6de34db1b819af
Pedro Gonoring <53987520+pedrogonoring1@users.noreply.github.com>
2020-06-14 00:54:14 -0300

    Primeiro up paradas proximas

---
+++ src/pages/ParadasProximas.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-page>
+
+    <!-- Title -->
+    <div class="row">
+        <div class="col-12 q-pt-lg q-mb-lg">
+            <label class="text-subtitle1 text-weight-bolder q-ml-lg q-mt-lg"> PARADAS PRÓXIMAS </label>
+        </div>
+    </div>  
+
+    <div class="row text-center">
+        <div class="col-4">
+            <q-img
+                src="~assets/img/hotel.png"
+                spinner-color="white"
+                style="height: 60px; max-width: 60px"
+                class="q-mb-sm"
+            /> <br>
+            <label for="">Hoteis</label>
+            
+        </div>
+        <div class="col-4">
+            s
+        </div>
+        <div class="col-4">
+            n
+        </div>
+    </div>
+    
+
+  
+    
+
+
+    
+
+
+
+  </q-page>
+</template>
+
+<script>
+
+export default {
+  name: 'PageParadasProximas'
+}
+</script>
