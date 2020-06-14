664fde00fab52f06898080025af922d56dd42dd0
Pedro Gonoring <53987520+pedrogonoring1@users.noreply.github.com>
2020-06-13 23:25:41 -0300

    Viagem interface

--- src/pages/Viagem.vue
+++ src/pages/Viagem.vue
@@ -1,9 +1,33 @@
 <template>
-  <q-page class="">
-    
-    <div class="userInfo flat">
-      
+  <q-page>
+
+    <div class="row paddingPanel">
+        <div class="col-12 userInfo">
+
+            <label class="text-weight-bolder"> 
+                VIAGEM ATUAL
+            </label> <br> <br>
+
+            <label class="text-weight-bolder"> 
+                Inicio:
+            </label>
+            <p> 
+                São Paulo, São Paulo.
+            </p> 
+
+            <label class="text-weight-bolder"> 
+                Destino:
+            </label> <br>
+            <label> 
+                Rio de Janeiro, Rio de Janeiro.
+            </label>
+
+
+            
+        </div>
     </div>
+    
+    
 
     
 
